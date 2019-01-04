b7b91ce89aa3887799bd65d9f1cc87b8fbfe03b9
Yuriy Plotnikov <yuriy.plotnikov@outlook.com>
2019-01-04 15:22:10 +0300

    Fixed settings.yaml, added accounts controller for regular addresses creation

---
+++ src/Lykke.Service.Nem.SignService/Controllers/AccountsController.cs
@@ -0,0 +1,23 @@
+﻿using io.nem1.sdk.Model.Accounts;
+using io.nem1.sdk.Model.Blockchain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lykke.Service.Nem.SignService.Controllers
+{
+    [ApiController]
+    [Route("/api/accounts")]
+    public class AccountsController : ControllerBase
+    {
+        [HttpPost("{network}")]
+        public object Create(string network)
+        {
+            var acc = Account.GenerateNewAccount(NetworkType.GetNetwork(network));
+
+            return new
+            {
+                acc.PrivateKey,
+                acc.Address
+            };
+        }
+    }
+}
